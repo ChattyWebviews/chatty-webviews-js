02cbde5709660332710ee1f500fbb7bcd6013f9c
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-03-02 11:49:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const handlers = {};
+
+let cwMessageHandler; 
+
+function attach() {
+    window['publish'] = (topic, message) => {
+        window.dispatchEvent(new CustomEvent('pubsub', {
+            detail: { topic, message },
+        }));
+    };
+
+    window['subscribe'] = (topic, handler) => {
+        const topicHandlers = handlers[topic] || [];
+        topicHandlers.push(handler);
+        handlers[topic] = topicHandlers;
+    };
+
+    window['unsubscribe'] = (topic, handler) => {
+        const topicHandlers = handlers[topic] || [];
+        const index = topicHandlers.indexOf(handler);
+        index >= 0 && topicHandlers.splice(index, 1);
+    };
+
+    window.addEventListener('pubsub', ev => {
+        const { topic, message } = ev['detail'];
+        const topicHandlers = handlers[topic] || [];
+        topicHandlers.forEach(handler => handler(message));
+    });
+}
+
+let subscribe = function(topic, callback) {
+    window['subscribe'](topic, msg => {
+      callback(msg);
+    });
+}
+
+function sendMessage(topic, message) {
+    if (window['webkit'] && window['webkit'].messageHandlers && window['webkit'].messageHandlers.cwMessageHandler) {
+            window['webkit'].messageHandlers.cwMessageHandler.postMessage({
+                "topic": topic,
+                "message": message
+            });
+    }
+
+    //push to web handlers too
+    window['publish'](topic, message);
+        
+}
+
+function isIos() {
+    console.log("CALLING")
+    return window['webkit'] && window['webkit'].messageHandlers && window['webkit'].messageHandlers.cwMessageHandler
+}
+
+module.exports = {
+    attach, 
+    subscribe,
+    sendMessage,
+    isIos
+}
